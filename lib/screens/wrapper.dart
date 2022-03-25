0b34090152b8c87a6abab4b9333bcf3f7ce7eeba
“aymenkhs” <aymenkhouas@gmail.com>
2022-03-25 05:56:02 +0100

    [add] initialize the project

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,16 @@
+import 'package:firstapp/screens/home/dashboard.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({ Key? key }) : super(key: key);
+
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return Dashboard();
+  }
+}
