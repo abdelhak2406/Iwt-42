3b6266157d2fbbd4b82dc81c8ac807abc000ddf1
“aymenkhs” <aymenkhouas@gmail.com>
2022-03-25 19:56:33 +0100

    added chart image

--- lib/screens/home/stats.dart
+++ lib/screens/home/stats.dart
@@ -10,8 +10,32 @@ class Stats extends StatefulWidget {
 class _StatsState extends State<Stats> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container  (
+          child: Image.asset('assets/images/chart.png'),
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10.0))
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Detailed',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
