373308e22e035a05ef5dcf03e4d199c3fa11770d
“aymenkhs” <aymenkhouas@gmail.com>
2022-03-25 18:20:36 +0100

    [feat] init stats

--- lib/screens/home/stats.dart
+++ lib/screens/home/stats.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+
+class Stats extends StatefulWidget {
+  const Stats({ Key? key }) : super(key: key);
+
+  @override
+  State<Stats> createState() => _StatsState();
+}
+
+class _StatsState extends State<Stats> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
