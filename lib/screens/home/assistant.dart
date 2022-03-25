1fe350411c080d2134f4b8335c91e3d219103462
“aymenkhs” <aymenkhouas@gmail.com>
2022-03-25 18:19:01 +0100

    [feat] init stats, predictions, assistant and goals

---
+++ lib/screens/home/assistant.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Assistant extends StatefulWidget {
+  const Assistant({ Key? key }) : super(key: key);
+
+  @override
+  State<Assistant> createState() => _AssistantState();
+}
+
+class _AssistantState extends State<Assistant> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/screens/home/predictions.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Predictions extends StatefulWidget {
+  const Predictions({ Key? key }) : super(key: key);
+
+  @override
+  State<Predictions> createState() => _PredictionsState();
+}
+
+class _PredictionsState extends State<Predictions> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
