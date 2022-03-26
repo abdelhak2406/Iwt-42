23e8784aba6fa56686dcabbda37dfafbdb524dd4
“aymenkhs” <aymenkhouas@gmail.com>
2022-03-26 04:21:51 +0100

    [feat] styling the body text in expandable pannels

--- lib/screens/home/goals.dart
+++ lib/screens/home/goals.dart
@@ -41,7 +41,16 @@ class _GoalsState extends State<Goals> {
   }
 
   Widget BodyTemplate() {
-    return Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        style: TextStyle(
+          color: Color(0xff39255C),
+          fontSize: 10,
+        ),
+      ),
+    );
   }
 
   @override
